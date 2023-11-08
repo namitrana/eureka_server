9d528bf2217cbe2ff56bedcfd36f0672f594ee73
nrana <namit_232@gmail.com>
2023-11-08 22:29:03 +0530

    added password for both the accounts.

--- src/main/java/com/example/demo/EurekaServerApplication.java
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -12,7 +12,9 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 		System.out.println("This is Eureka Server...");
-		//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  - password
+		//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 - namit232@gmail.com password
+		//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 - namit_232@yahoo.com password 
+		//test
 	}
 
 }
